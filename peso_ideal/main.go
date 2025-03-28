318d2c9b5565be86df47d8d628f29308a3f63c2e
Kauã Gomes <kauagomez493@gmail.com>
2025-03-28 09:04:10 -0300

    Adicionando nova tarefa

--- peso_ideal/main.go
+++ peso_ideal/main.go
@@ -30,25 +30,13 @@ func main() {
 	fmt.Printf("Seu IMC é: %.2f\n", IMC)
 
 	
-	if sexo == "M" {
-		if IMC < 20 {
-			fmt.Println("Você está abaixo do peso.")
-		} else if IMC >= 20 && IMC < 25 {
-			fmt.Println("Seu peso está normal.")
-		} else if IMC >= 25 && IMC < 30 {
-			fmt.Println("Você está com sobrepeso.")
-		} else {
-			fmt.Println("Você está com obesidade.")
-		}
-	} else if sexo == "F" { 
-		if IMC < 19 {
-			fmt.Println("Você está abaixo do peso.")
-		} else if IMC >= 19 && IMC < 24 {
-			fmt.Println("Seu peso está normal.")
-		} else if IMC >= 24 && IMC < 29 {
-			fmt.Println("Você está com sobrepeso.")
-		} else {
-			fmt.Println("Você está com obesidade.")
-		}
-	}
-}
+	if IMC < 18.5 {
+		fmt.Println("Você está abaixo do peso.")
+	} else if IMC < 25 {
+		fmt.Println("Seu peso está normal.")
+	} else if IMC < 30 {
+		fmt.Println("Você está com sobrepeso.")
+	} else {
+		fmt.Println("Você está com obesidade.")
+	}	
+}	
